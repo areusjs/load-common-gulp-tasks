4f73f221250a9e6b9e996897cdb14760cbbb0228
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-04-10 10:59:36 -0500

    separate target for felint

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ var jshint = require('gulp-jshint'),
 module.exports = function (gulp) {
 
   gulp.task('lint', function () {
-    gulp.src(['gulpfile.js', 'lib/**/*.js', '!**/coverage/lcov-report/**'])
-      .pipe(jshint())
+    gulp.src([
+      'gulpfile.js',
+      'lib/**/*.js',
+      '!lib/*/coverage/**',
+      '!lib/*/content/**'
+    ])
+      .pipe(jshint('./node_modules/load-common-gulp-tasks/lint/.jshintrc'))
       .pipe(jshint.reporter(stylish));
-      //.pipe(jshint.reporter('fail')); // fails on first encountered error instead of running full report.
+    //.pipe(jshint.reporter('fail')); // fails on first encountered error instead of running full report.
   });
+
+  gulp.task('felint', function () {
+    gulp.src(['lib/*/content/**/*.js'])
+      .pipe(jshint('./node_modules/load-common-gulp-tasks/felint/.jshintrc'))
+      .pipe(jshint.reporter(stylish));
+    //.pipe(jshint.reporter('fail')); // fails on first encountered error instead of running full report.
+  });
+
+  gulp.task('default', ['lint', 'felint']);
 };
