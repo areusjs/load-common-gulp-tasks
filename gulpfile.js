cc6278e29e0ad004f1820ee5722cad581991e9e2
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-04-10 10:00:30 -0500

    use itself to perform lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,2 @@
 var gulp = require('gulp');
+require('./index.js')(gulp);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var jshint = require('gulp-jshint'),
 module.exports = function (gulp) {
 
   gulp.task('lint', function () {
-    gulp.src(['lib/**/*.js', 'test/**/*.js'])
+    gulp.src(['gulpfile.js', 'lib/**/*.js', 'test/**/*.js'])
       .pipe(jshint())
       .pipe(jshint.reporter(stylish));
       //.pipe(jshint.reporter('fail')); // fails on first encountered error instead of running full report.
