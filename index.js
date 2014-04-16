1dad0427e88c90d5bf74e76e483f9430f76fa6a9
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-04-16 14:37:38 -0500

    reorg test targets. use new gulp-help version

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ module.exports = function (gulp, options) {
     }
   }
 
-  gulp.task('lint', 'Lint server side js and fails on error', function () {
+  gulp.task('lint', 'Lint server side js', function () {
     return lint()
       .on('end', function () {
         lintOnEnd();
@@ -126,7 +126,7 @@ module.exports = function (gulp, options) {
       }));
   });
 
-  gulp.task('lint-watch', 'Show full server side js lint report without failing', function () {
+  gulp.task('lint-watch', false, function () {
     return lint()
       .on('end', lintOnEnd)
       .pipe(size({
@@ -162,7 +162,7 @@ module.exports = function (gulp, options) {
     }
   }
 
-  gulp.task('felint', 'Lint client side js and fail on error', function () {
+  gulp.task('felint', 'Lint client side js', function () {
     return felint()
       .on('end', function () {
         felintOnEnd();
@@ -175,7 +175,7 @@ module.exports = function (gulp, options) {
       }));
   });
 
-  gulp.task('felint-watch', 'Show full client side js lint without failing', function () {
+  gulp.task('felint-watch', false, function () {
     return felint()
       .on('end', felintOnEnd)
       .pipe(size({
@@ -203,7 +203,7 @@ module.exports = function (gulp, options) {
       }));
   }
 
-  gulp.task('test', 'Unit tests and coverage', function (cb) {
+  gulp.task('test-cover', 'Unit tests and coverage', function (cb) {
     return cover(function () {
       gulp.src(gulp.options.paths.test)
         .pipe(mocha({reporter: 'dot'}))
@@ -212,7 +212,7 @@ module.exports = function (gulp, options) {
           process.emit('exit');
         })
         .pipe(size({
-          title: 'test'
+          title: 'test-cover'
         }))
         .pipe(istanbul.writeReports(gulp.options.coverageSettings.coverageDirectory))
         .pipe(coverageEnforcer(gulp.options.coverageSettings))
@@ -224,34 +224,61 @@ module.exports = function (gulp, options) {
     });
   });
 
-  gulp.task('test-watch', function (cb) {
+  gulp.task('test-cover-watch', false, function (cb) {
     return cover(function () {
       gulp.src(gulp.options.paths.test)
-        .pipe(mocha({
-          reporter: 'min',
-          G: true
-        }))
+        .pipe(mocha({reporter: 'dot'}))
         .on('error', testErrorHandler) // handler for mocha error
+        .pipe(size({
+          title: 'test-cover'
+        }))
+        .pipe(istanbul.writeReports(gulp.options.coverageSettings.coverageDirectory))
+        .pipe(coverageEnforcer(gulp.options.coverageSettings))
+        .on('error', testErrorHandler) // handler for istanbul error
         .on('end', cb);
     });
+  });
+
+  gulp.task('test', 'Unit tests only', function () {
+    return gulp.src(gulp.options.paths.test)
+      .pipe(mocha({reporter: 'dot'}))
+      .on('error', function (err) { // handler for mocha error
+        testErrorHandler(err);
+        process.emit('exit');
+      })
+      .pipe(size({
+        title: 'test'
+      }));
+  });
+
+  gulp.task('test-watch', false, function (cb) {
+    return gulp.src(gulp.options.paths.test)
+      .pipe(mocha({
+        reporter: 'min',
+        G: true
+      }))
+      .on('error', testErrorHandler) // handler for mocha error
+      .pipe(size({
+        title: 'test-watch'
+      }));
   });
 
   // ----------------
   // combo tasks
   // ----------------
 
-  gulp.task('ci', 'All lint and tests', ['lint', 'felint', 'test']);
+  gulp.task('ci', 'Lint, tests and test coverage', ['lint', 'felint', 'test-cover']);
 
-  gulp.task('ci-watch', ['lint-watch', 'felint-watch', 'test-watch']);
+  gulp.task('ci-watch', false, ['lint-watch', 'felint-watch', 'test-cover-watch']);
 
-  gulp.task('watch-all', 'Watch files and run ci', function () {
+  gulp.task('watch-all', 'Watch files and run all ci validation on change', function () {
     gulp.watch(gulp.options.paths.lint, ['ci-watch']);
   });
 
-  gulp.task('watch', function () {
+  gulp.task('watch', 'Watch files and run tests on change', function () {
     gulp.watch(gulp.options.paths.lint, ['test-watch']);
   });
 
-  gulp.task('default', ['watch']);
+  gulp.task('default', false, ['watch']);
 
 };
