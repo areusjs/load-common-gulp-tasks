2bdc4cd9b9440719e1d01db67a2eebe52db9b94b
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-10-29 12:28:26 -0500

    fix tests to work in travis and report better

--- test/index-integration-test.js
+++ test/index-integration-test.js
@@ -9,6 +9,8 @@ var spawn = require('child_process').spawn,
 
 describe('integration', function () {
 
+  this.timeout(9000);
+
   function printSpawnOutput(spawned) {
     //spawned.stdout.pipe(process.stdout);
     //spawned.stderr.pipe(process.stderr);
@@ -23,19 +25,23 @@ describe('integration', function () {
       var gulpSpawn = spawn('gulp', ['ci'], {cwd: examplePath});
       printSpawnOutput(gulpSpawn);
       gulpSpawn.on('close', function (code) {
-        should(code).eql(0);
-        var coverageJson = require(path.join(examplePath, 'target/coverage/coverage-final.json'));
-        var keys = Object.keys(coverageJson);
-        should(keys.length).eql(2);
-        keys.forEach(function (key) {
-          /* jshint noempty: false */
-          if (path.basename(key) === 'square.js' ||
-            path.basename(key) === 'root.js') {
-          } else {
-            throw new Error('Unexpected file in coverage report: ' + key);
-          }
-          /* jshint noempty: true */
-        });
+        try {
+          should(code).eql(0);
+          var coverageJson = require(path.join(examplePath, 'target/coverage/coverage-final.json'));
+          var keys = Object.keys(coverageJson);
+          should(keys.length).eql(2);
+          keys.forEach(function (key) {
+            /* jshint noempty: false */
+            if (path.basename(key) === 'square.js' ||
+              path.basename(key) === 'root.js') {
+            } else {
+              throw new Error('Unexpected file in coverage report: ' + key);
+            }
+            /* jshint noempty: true */
+          });
+        } catch (e) {
+          return done(e);
+        }
         done();
       });
 
@@ -51,7 +57,11 @@ describe('integration', function () {
       var gulpSpawn = spawn('gulp', ['ci'], {cwd: examplePath});
       printSpawnOutput(gulpSpawn);
       gulpSpawn.on('close', function (code) {
-        should(code).eql(1);
+        try {
+          should(code).eql(1);
+        } catch (e) {
+          return done(e);
+        }
         done();
       });
     });
@@ -60,7 +70,11 @@ describe('integration', function () {
       var gulpSpawn = spawn('gulp', ['lint'], {cwd: examplePath});
       printSpawnOutput(gulpSpawn);
       gulpSpawn.on('close', function (code) {
-        should(code).eql(1);
+        try {
+          should(code).eql(1);
+        } catch (e) {
+          return done(e);
+        }
         done();
       });
 
@@ -70,7 +84,11 @@ describe('integration', function () {
       var gulpSpawn = spawn('gulp', ['felint'], {cwd: examplePath});
       printSpawnOutput(gulpSpawn);
       gulpSpawn.on('close', function (code) {
-        should(code).eql(1);
+        try {
+          should(code).eql(1);
+        } catch (e) {
+          return done(e);
+        }
         done();
       });
 
@@ -80,7 +98,11 @@ describe('integration', function () {
       var gulpSpawn = spawn('gulp', ['test'], {cwd: examplePath});
       printSpawnOutput(gulpSpawn);
       gulpSpawn.on('close', function (code) {
-        should(code).eql(0);
+        try {
+          should(code).eql(0);
+        } catch (e) {
+          return done(e);
+        }
         done();
       });
 
@@ -90,19 +112,23 @@ describe('integration', function () {
       var gulpSpawn = spawn('gulp', ['test-cover'], {cwd: examplePath});
       printSpawnOutput(gulpSpawn);
       gulpSpawn.on('close', function (code) {
-        should(code).eql(1);
-        var coverageJson = require(path.join(examplePath, 'target/coverage/coverage-final.json'));
-        var keys = Object.keys(coverageJson);
-        should(keys.length).eql(2);
-        keys.forEach(function (key) {
-          /* jshint noempty: false */
-          if (path.basename(key) === 'square.js' ||
-            path.basename(key) === 'root.js') {
-          } else {
-            throw new Error('Unexpected file in coverage report: ' + key);
-          }
-          /* jshint noempty: true */
-        });
+        try {
+          should(code).eql(1);
+          var coverageJson = require(path.join(examplePath, 'target/coverage/coverage-final.json'));
+          var keys = Object.keys(coverageJson);
+          should(keys.length).eql(2);
+          keys.forEach(function (key) {
+            /* jshint noempty: false */
+            if (path.basename(key) === 'square.js' ||
+              path.basename(key) === 'root.js') {
+            } else {
+              throw new Error('Unexpected file in coverage report: ' + key);
+            }
+            /* jshint noempty: true */
+          });
+        } catch (e) {
+          return done(e);
+        }
         done();
       });
 
